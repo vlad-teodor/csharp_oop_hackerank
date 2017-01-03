895cf9f4b7bd0faaa22036f5493837284392c07f
Vlad Teodor <vladteodor@outlook.com>
2017-01-03 18:43:40 +0200

    Method Overriding 1 & 2 done.

---
+++ overriding1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overriding1
+{
+    class Sports
+    {
+        public string getName()
+        {
+            return "Generic Sports";
+        }
+
+        public void getNumberOfTeamMembers()
+        {
+            Console.WriteLine("Each team has n players in " + getName());
+        }
+    }
+    
+    class Soccer : Sports
+    {
+        public new string getName()
+        {
+            return "Soccer class";
+        }
+
+        public new void getNumberOfTeamMembers()
+        {
+            Console.WriteLine("Each team has 11 players in " + getName());
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sports c1 = new Sports();
+            Soccer c2 = new Soccer();
+            Console.WriteLine(c1.getName());
+            c1.getNumberOfTeamMembers();
+            Console.WriteLine(c2.getName());
+            c2.getNumberOfTeamMembers();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ overriding2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overriding2
+{
+    class BiCycle
+    {
+        protected string defineMe()
+        {
+            return "a vehicle with pedals.";
+        }
+    }
+
+    class MotorCycle : BiCycle
+    {
+        string defineMe()
+        {
+            return "a cycle with an engine";
+        }
+        
+        public MotorCycle()
+        {
+            Console.WriteLine("Hello I am motorcycle, I am " + defineMe());
+            string temp = base.defineMe();
+            Console.WriteLine("My ancestor is a cycle who is " + temp);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MotorCycle M = new MotorCycle();
+
+            Console.ReadKey();
+        }
+    }
+}
