c624e39032d71d8253857871b010ad7580085eee
Vlad Teodor <vladteodor@outlook.com>
2017-01-03 17:44:39 +0200

    inheritance 1 & 2

--- inheritance1/Program.cs
+++ inheritance1/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace inheritance1
 {
+    class Animal
+    {
+        public void walk()
+        {
+            Console.WriteLine("I am walking");
+        }
+    }
+
+    class Bird : Animal
+    {
+        public void fly()
+        {
+            Console.WriteLine("I am flying");
+        }
+
+        public void sing()
+        {
+            Console.WriteLine("I am singing");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Bird bird = new Bird();
+            bird.walk();
+            bird.fly();
+            bird.sing();
+
+            Console.ReadKey(); // sa nu se inchida consola instant
         }
     }
 }
---
+++ inheritance2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance2
+{
+    class Arithmetic
+    {
+        public int add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    class Adder : Arithmetic
+    {
+        public void printSuperclass()
+        {
+            Console.WriteLine("My superclass is: " + this.GetType().BaseType.Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adder adder = new Adder();
+
+            adder.printSuperclass();
+            Console.WriteLine(adder.add(30, 12) + " " + adder.add(10, 3) + " " + adder.add(30, -10));
+
+            Console.ReadKey();
+        }
+    }
+}
